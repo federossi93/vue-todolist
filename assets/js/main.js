83b88f5d5ca9e2d5919bc06d51463ac445c0139a
federossi93 <rossifederico0993@gmail.com>
2022-11-01 09:52:55 +0100

    end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,47 @@
-console.log("hello");
 
+const { createApp } = Vue
 
-
-
-    import {createApp} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-
-
-    createApp({
-        data() {
+createApp({
+    data() {
         return {
-        message: 'Hello Vue!'
+            error: false,
+            todoList: [
+                {
+                    text: 'Andare a cena con amici',
+                    done: false
+                },
+                {
+                    text: 'Pulire casa',
+                    done: false
+                },
+                {
+                    text: 'Pulire il giardino',
+                    done: false
+                },
+                {
+                    text: 'Pulire la macchina',
+                    done: false
+                },
+            ]
+        }
+    },
+    methods: {
+        done(i) {
+            this.todoList[i].done = !this.todoList[i].done
+        },
+        test(i) {
+            console.log(this.todoList[i].text);
+            this.todoList.splice(i, 1)
+        },
+        addNew() {
+            if (this.newTask.length < 5) {
+                this.error = true
+            } else {
+                const obj = { text: this.newTask, done: false }
+                this.todoList.unshift(obj)
+                this.newTask = ""
+            }
+
         }
     }
 }).mount('#app')
